962db4e0fcdb76e408e2cdc38979f69c41345fdd
Avishek-45 <theabhi45@gmail.com>
2020-11-08 21:10:00 +0545

    Events and dynamic css

--- Bascis/app.js
+++ Bascis/app.js
@@ -1,21 +1,35 @@
 new Vue({
-    el: '#vue_app',
-    data: {
-        name: 'Avishek',
-        job: 'Web developer',
-        website: 'http://avishekguragain.com.np',
-        X:0,
-        Y:0,
-        number:100
-
+  el: "#vue_app",
+  data: {
+    name: "Avishek",
+    job: "Web developer",
+    website: "http://avishekguragain.com.np",
+    X: 0,
+    Y: 0,
+    number: 100,
+    available:false,
+    nearby:false,
+  },
+  methods: {
+    Coordinates: function (event) {
+      this.X = event.offsetX;
+      this.Y = event.offsetY;
     },
-    methods: {
-        greet:function(){
-            return 'Hello'
-        },
-        Coordinates:function(event){
-                this.X=event.offsetX
-                this.Y=event.offsetY
+    enter: function () {
+      console.log("Helllo");
+    },
+  },
+  computed: {
+    greet: function () {
+      return "Hello";
+    },
+
+    dynamicCSS: function () {
+        return {
+            
+            available:this.available,
+            nearby:this.nearby
         }
     }
-})
+  },
+});
